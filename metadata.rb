890c7acf1916d21ea537c09127ddd5601725e7d7
Jake Briggs <briggs679@gmail.com>
2013-09-26 13:37:01 -0500

    No blind revisions

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'wse@rackspace.com'
 license          'All rights reserved!'
 description      'Installs/Configures sysedge'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.4'
+version          '0.3.5'
